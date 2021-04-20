38ce3af0f2fc428c4c9d070c59070c8c9a460f93
milanhirani27 <milan.170410116023@gmail.com>
2021-04-20 10:01:55 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,211 @@
+const express = require("express");
+const logger = require('morgan');
+const http = require('http')
+const socketIo = require('socket.io')
+const needle = require('needle')
+const config = require('dotenv').config()
+const TOKEN = process.env.TWITTER_BEARER_TOKEN;
+const expressLayouts = require('express-ejs-layouts');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const path = require('path');
+const ejs = require('ejs')
+const app = express();
+
+//port init
+const port = process.env.NODE_ENV || 3000;
+
+//server
+const server = http.createServer(app)
+const io = socketIo(server)
+
+// URL
+const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules'
+const streamURL =
+  'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id'
+
+const rules = [{ value: 'giveaway' }]
+
+// Get stream rules
+async function getRules() {
+  const response = await needle('get', rulesURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+  console.log(response.body)
+  return response.body
+}
+
+// Set stream rules
+async function setRules() {
+  const data = {
+    add: rules,
+  }
+
+  const response = await needle('post', rulesURL, data, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+
+  return response.body
+}
+
+// Delete stream rules
+async function deleteRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null
+  }
+
+  const ids = rules.data.map((rule) => rule.id)
+
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  }
+
+  const response = await needle('post', rulesURL, data, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+
+  return response.body
+}
+
+function streamTweets(socket) {
+  const stream = needle.get(streamURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+
+  stream.on('data', (data) => {
+    try {
+      const json = JSON.parse(data)
+      console.log(json)
+      socket.emit('tweet', json)
+    } catch (error) {}
+  })
+
+  return stream
+}
+
+io.on('connection', async () => {
+  console.log('Client connected...')
+
+  let currentRules
+
+  try {
+    //   Get all stream rules
+    currentRules = await getRules()
+
+    // Delete all stream rules
+    await deleteRules(currentRules)
+
+    // Set rules based on array above
+    await setRules()
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+
+  const filteredStream = streamTweets(io)
+
+  let timeout = 0
+  filteredStream.on('timeout', () => {
+    // Reconnect on error
+    console.warn('A connection error occurred. Reconnecting…')
+    setTimeout(() => {
+      timeout++
+      streamTweets(io)
+    }, 2 ** timeout)
+    streamTweets(io)
+  })
+})
+
+
+//database connection
+require("./models/db");
+
+//routes
+const router = require('./router/index');
+
+//express bodyparser
+app.use(express.json());
+
+// EJS
+app.use(expressLayouts);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
+// Express session
+app.use(
+  session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// passport config
+require('./config/passport')(passport);
+
+//web user
+app.use('/',router);
+app.use('/users', require('./router/users'));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+  });
+
+  app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
+  
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+  });
+
+//listen to port no 3000
+server.listen(port , ()=>{
+    console.log(`App running on port ${port}`)
+})
